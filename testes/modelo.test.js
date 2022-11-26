1863dbfc67c3594d917252d6cb6791813b76665a
Isabel Elise <isaelised@gmail.com>
2022-11-25 22:15:23 -0300

    Atualização dos testes de integração para cobertura de 100%

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -23,3 +23,17 @@ test('Testando cadastro de três perguntas', () => {
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
 });
+
+test('Testando cadastro de três respostas', () => {
+  modelo.cadastrar_pergunta('1 + 1 = ?');
+  const perguntas = modelo.listar_perguntas(); 
+  const pergunta = modelo.get_pergunta(perguntas[0].id_pergunta);
+  modelo.cadastrar_resposta(pergunta.id_pergunta, '2');
+  modelo.cadastrar_resposta(pergunta.id_pergunta, '3');
+  modelo.cadastrar_resposta(pergunta.id_pergunta, '4');
+  const respostas = modelo.get_respostas(pergunta.id_pergunta); 
+  expect(respostas.length).toBe(3);
+  expect(respostas[0].texto).toBe('2');
+  expect(respostas[1].texto).toBe('3');
+  expect(respostas[1].id_resposta).toBe(respostas[2].id_resposta-1);
+});
